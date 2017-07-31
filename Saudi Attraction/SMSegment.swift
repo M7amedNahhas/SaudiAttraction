0e65070ed641dba76c9f5f160c914175441943dd
M7amedNahhas <m7amednahhas@gmail.com>
2017-07-31 13:31:23 +0300

    new adding

---
+++ Saudi Attraction/SMSegment.swift
@@ -0,0 +1,18 @@
+//
+//  SMSegment.swift
+//  Saudi Attraction
+//
+//  Created by M7amedNahhas on 11/8/1438 AH.
+//  Copyright © 1438 WadiMakkah. All rights reserved.
+//
+
+import UIKit
+
+
+protocol SMSegment {
+    var segmentName: String? {get}
+    var segmentImage: UIImage?{get}
+    var segmentSelectedImage: UIImage? { get }
+    
+    
+}
