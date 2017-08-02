ecfb6549ea8a7fdd59c92b4ca0b8fad59bbdc0e5
عبدالرحمن الفارس <AbdulrahmanAlfares@MAC--AF1994.local>
2017-08-02 14:55:09 +0300

    search bar

---
+++ Saudi Attraction/DataContainer.swift
@@ -0,0 +1,9 @@
+//
+//  DataContainer.swift
+//  Saudi Attraction
+//
+//  Created by عبدالرحمن الفارس on ٩ ذو. ق، ١٤٣٨ هـ.
+//  Copyright © ١٤٣٨ هـ WadiMakkah. All rights reserved.
+//
+
+import Foundation
---
+++ Saudi Attraction/SMRegionManager.swift
@@ -0,0 +1,9 @@
+//
+//  SMRegionManager.swift
+//  Saudi Attraction
+//
+//  Created by عبدالرحمن الفارس on ١٠ ذو. ق، ١٤٣٨ هـ.
+//  Copyright © ١٤٣٨ هـ WadiMakkah. All rights reserved.
+//
+
+import Foundation
